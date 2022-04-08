6eff1f1ee98678696daa34a858eaeef4eae3ccbf
ValiaStohnii <valia19961018@gmail.com>
2022-04-08 20:24:40 +0300

    add private route

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,43 @@
-import { Routes, Route } from 'react-router-dom';
-import RegisterPage from 'pages/RegisterPage';
-import LoginPage from 'pages/LoginPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './organisms/layout';
-import ContactsPage from 'pages/ContactsPage';
-import HomePage from 'pages/HomePage';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, Suspense, lazy } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import authOperations from 'redux/auth/auth-operations';
+import authSelectors from 'redux/auth/auth-selectors';
+
+const HomePage = lazy(() => import('pages/HomePage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const ContactsPage = lazy(() => import('pages/ContactsPage'));
 
 function App() {
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="contacts" element={<ContactsPage />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<h1>Loadig...</h1>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="register"
+            element={isLoggedIn ? <HomePage /> : <RegisterPage />}
+          />
+          <Route
+            path="login"
+            element={isLoggedIn ? <HomePage /> : <LoginPage />}
+          />
+          <Route
+            path="contacts"
+            element={isLoggedIn ? <ContactsPage /> : <Navigate to="/login" />}
+          />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import authSelectors from 'redux/auth/auth-selectors';
 
-const HomePage = () => (
-  <div>
-    <h1>WELCOME </h1>
-  </div>
-);
+const HomePage = () => {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  return (
+    <div>{isLoggedIn ? <h1>WELCOME</h1> : <h1>Log In or Sing Up </h1>}</div>
+  );
+};
 
 export default HomePage;
