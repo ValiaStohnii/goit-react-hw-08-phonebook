bc8b7e94210750d66bbca77e6765b237668fe3a0
ValiaStohnii <valia19961018@gmail.com>
2022-04-08 14:00:19 +0300

    fix baseurl

--- src/redux/slice/contactsSlice.js
+++ src/redux/slice/contactsSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: '/',
+    baseUrl: 'https://connections-api.herokuapp.com/',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
