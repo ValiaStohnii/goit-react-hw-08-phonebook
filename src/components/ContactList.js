f442f3748e14f1010cc987b492eef0b5975476e7
ValiaStohnii <valia19961018@gmail.com>
2022-04-10 11:19:53 +0300

    styles contacts list

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,41 +1,14 @@
 import ContactListItem from './ContactListItem';
+import List from '@mui/material/List';
 
 const ContactList = ({ contacts }) => {
   return (
-    <ul>
+    <List sx={{ width: '100%', maxWidth: 360 }}>
       {contacts.map(contacts => (
         <ContactListItem key={contacts.id} {...contacts} />
       ))}
-    </ul>
+    </List>
   );
 };
 
 export default ContactList;
-
-// import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import contactsAction from '../redux/contacts/contacts-actions';
-// import { getVisibleContacts } from '../redux/contacts/contacts-selectors';
-
-// const ContactList = () => {
-//   const contacts = useSelector(getVisibleContacts);
-//   const dispatch = useDispatch();
-
-//   const onDeleteContact = id => dispatch(contactsAction.deleteContact(id));
-
-//   return (
-//     <ul>
-//       {contacts.map(contacts => (
-//         <li key={contacts.id} name={contacts.name}>
-//           <p>{contacts.name}</p>
-//           <p>{contacts.number}</p>
-//           <button type="button" onClick={() => onDeleteContact(contacts.id)}>
-//             Delete
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default ContactList;
--- src/components/ContactListItem.js
+++ src/components/ContactListItem.js
@@ -1,15 +1,36 @@
 import { useDeleteContactMutation } from 'redux/slice/contactsSlice';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Avatar from '@mui/material/Avatar';
+import theme from './theme/theme';
 
 const ContactListItem = ({ id, name, number }) => {
-  const [deleteContact, { isLoading: isDeleting }] = useDeleteContactMutation();
+  const [deleteContact] = useDeleteContactMutation();
   return (
-    <li name={name}>
-      <p>{name}</p>
-      <p>{number}</p>
-      <button type="button" onClick={() => deleteContact(id)}>
-        {isDeleting ? 'Deleting...' : 'Delete'}
-      </button>
-    </li>
+    <ListItem
+      secondaryAction={
+        <IconButton
+          type="button"
+          onClick={() => deleteContact(id)}
+          edge="end"
+          aria-label="delete"
+        >
+          <DeleteIcon theme={theme} color="secondary" />
+        </IconButton>
+      }
+      name={name}
+    >
+      <ListItemAvatar>
+        <Avatar sx={{ bgcolor: '#2f2f3f' }} alt={name} src="/broken-image.jpg">
+          {/* <FolderIcon /> */}
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={name} />
+      <ListItemText primary={number} />
+    </ListItem>
   );
 };
 
